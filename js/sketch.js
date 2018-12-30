d44c8f8fbea58e7badf6edb4ea933b9a5c7ed48a
Harry <harryrock67@gmail.com>
2018-12-29 21:28:50 -0500

    sexxxyyyyy

--- js/sketch.js
+++ js/sketch.js
@@ -79,7 +79,7 @@ function draw() {
         for (let i in cores) {
             cores[i].show();
             //            cores[i].update(freq[Math.floor(200 / (i + 1))]);
-            cores[i].update(vol * 100);
+            cores[i].update(vol * ((i + 1) * 100));
         }
     }
 }
