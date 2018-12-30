130d134a3b6e289cf3dcaa90bfe803e4521226f1
Harry <harryrock67@gmail.com>
2018-12-29 19:00:27 -0500

    Added update and draw for creapears and functionality to crawl
    Also catches them in the center
    Stroke is updated randomly

--- js/Creeper.js
+++ js/Creeper.js
@@ -1,16 +1,63 @@
 class Creeper {
-    construtor(x, y) {
-        this.pos = createVector(x, y);
-        this.step = createVector(10, 10);
+    constructor(x, y) {
+        this.center = {
+            x: width / 2,
+            y: height / 2
+        };
+        this.pos = {
+            x: x,
+            y: y
+        };
+        this.step = {
+            x: x,
+            y: y
+        };
+        this.dist = {
+            x: 0,
+            y: 0
+        }
+        this.totalSteps = 20;
+        this.life = 100;
+        this.chance = 100;
+        this.core = {
+            w: 20,
+            h: 20
+        }
     }
     show() {
-        stroke(255);
-        strokeWeight(2);
         noFill();
         line(this.pos.x, this.pos.y, this.step.x, this.step.y);
+        this.pos.x = this.step.x;
+        this.pos.y = this.step.y;
+        this.update();
     }
     update() {
-        //        this.step.x = 10;
-        //        this.step.y = 10;
+        let chance = Math.floor(random(this.chance)) == 1;
+        if (chance) {
+            this.updateDist();
+            this.assignColor();
+            if (this.catch(this.pos)) {} else {
+                this.step.x = random(this.pos.x, this.pos.x + (this.dist.x / this.totalSteps));
+                this.step.y = random(this.pos.y, this.pos.y + (this.dist.y / this.totalSteps));
+            }
+        }
+    }
+    catch (pos) {
+        let coreRange = {
+            x: this.core.w / 2,
+            y: this.core.h / 2
+        }
+        if ((pos.x > this.center.x - coreRange.x && pos.x < this.center.x + coreRange.x) || (pos.y > this.center.y - coreRange.y && pos.y < this.center.y + coreRange.y)) {
+            return true
+        } else {
+            return false
+        }
+    }
+    updateDist() {
+        this.dist.x = this.center.x - this.pos.x;
+        this.dist.y = this.center.y - this.pos.y;
+    }
+    assignColor() {
+        stroke(col[Math.floor(random(5))].r, col[Math.floor(random(5))].g, col[Math.floor(random(5))].b);
     }
 }
--- js/sketch.js
+++ js/sketch.js
@@ -1,4 +1,7 @@
-let creepers = [],
+let creepersLeft = [],
+    creepersTop = [],
+    creepersRight = [],
+    creepersBottom = [],
     col = {
         0: {
             r: 0,
@@ -6,40 +9,46 @@ let creepers = [],
             b: 0
         },
         1: {
-            r: 0,
-            g: 0,
-            b: 0
+            r: 49,
+            g: 73,
+            b: 60
         },
         2: {
-            r: 0,
-            g: 0,
-            b: 0
+            r: 122,
+            g: 158,
+            b: 126
         },
         3: {
-            r: 0,
-            g: 0,
-            b: 0
+            r: 124,
+            g: 53,
+            b: 69
         },
         4: {
-            r: 0,
-            g: 0,
-            b: 0
-        }
+            r: 1,
+            g: 23,
+            b: 47
+        },
     };
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight, P2D);
     for (let i = 0; i < 20; i++) {
-        creepers[i] = new Creeper(0, 0);
+        creepersLeft[i] = new Creeper(0, random(0, height));
+        creepersTop[i] = new Creeper(random(0, width), 0);
+        creepersRight[i] = new Creeper(width, random(0, height));
+        creepersBottom[i] = new Creeper(random(0, width), height);
     }
+    background(col[0].r, col[0].g, col[0].b);
 }
 
 function draw() {
-    background(col[0].r, col[0].g, col[0].b);
+    background(col[0].r, col[0].g, col[0].b, 10);
 
-    for (let i in creepers) {
-        creepers[i].show();
-        creepers[i].update();
+    for (let i in creepersLeft) {
+        creepersLeft[i].show();
+        creepersTop[i].show();
+        creepersRight[i].show();
+        creepersBottom[i].show();
     }
 }
 
